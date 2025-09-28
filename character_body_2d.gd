b12eb26c754fe2540cd1ade821cce08193924678
michellejiangmy-web <michelle.jiangmy@gmail.com>
2025-09-28 02:46:04 -0400

    vgwrf

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
+@onready var _animated_sprite = $AnimatedSprite2D
 
 const SPEED = 900.0
 const JUMP_VELOCITY = -900.0
-@onready var _animated_sprite = $AnimatedSprite2D
 
 
+func _process(_delta):
+	if Input.is_action_pressed("right") or Input.is_action_pressed("left"):
+		_animated_sprite.play("walk")
+	else:
+		_animated_sprite.play("idle")
+		
 func _physics_process(delta: float) -> void:
-	
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -24,30 +28,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-func _process(delta):
-	# Animate sprite based on velocity.x
-	if velocity.x != 0:
-		_animated_sprite.play("run")
-		_animated_sprite.flip_h = velocity.x < 0  # Flip if moving left
-	else:
-		_animated_sprite.play("idle")
+
 	move_and_slide()
-	#$Timer.start()
-
-
-
-	# Input.is_action_pressed("right"):
-		#_animated_sprite.play("run")
-	#else:
-		#_animated_sprite.stop()
-		
-#func _on_timer_timeout() -> void:
-	#$Timer.timeout.connect(_on_timer_timeout)
-	#print("ok time over")
-
-#func _process(delta):
-	#if $CountdownTimer.time_left > 0:
-		#$CountdownLabel.text = str(round($CountdownTimer.time_left))
-	#else:
-		#$CountdownLabel.text = "GO!"  # Or hide it
